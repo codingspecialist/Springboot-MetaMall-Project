44bfad59c4ce96df6c6158ca2cd8429dda991cc7
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-04-09 12:09:11 +0900

    Feat: Add findById(product)

--- src/main/java/shop/mtcoding/metamall/controller/ProductController.java
+++ src/main/java/shop/mtcoding/metamall/controller/ProductController.java
@@ -2,10 +2,7 @@ package shop.mtcoding.metamall.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.metamall.core.exception.Exception404;
 import shop.mtcoding.metamall.dto.ResponseDto;
 import shop.mtcoding.metamall.dto.product.ProductRequest;
@@ -43,10 +40,23 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public ResponseEntity<?> getAll() {
+    public ResponseEntity<?> findAll() {
         List<Product> productListPS = productRepository.findAll();
 
         ResponseDto<?> responseDto = new ResponseDto<>().data(productListPS);
         return ResponseEntity.ok().body(responseDto);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<?> findById(@PathVariable Integer id) {
+        Optional<Product> productPS = productRepository.findById(id.longValue());
+        if (productPS.isPresent()) {
+            Product findProduct = productPS.get();
+
+            ResponseDto<?> responseDto = new ResponseDto<>().data(findProduct);
+            return ResponseEntity.ok().body(responseDto);
+        } else {
+            throw new Exception404("존재하지 않는 상품입니다.");
+        }
     }
 }
