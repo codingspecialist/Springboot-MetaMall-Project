cb5a50a305e3f47765a437272168c69ce4f9857d
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-12 15:19:57 +0900

    FEAT: add entity architecture

--- src/main/java/shop/mtcoding/metamall/model/user/User.java
+++ src/main/java/shop/mtcoding/metamall/model/user/User.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.metamall.model.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,33 +15,61 @@ import java.time.LocalDateTime;
 @Table(name = "user_tb")
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
     private String username;
+
+    @JsonIgnore
+    @Column(nullable = false, length = 60)
     private String password;
+
+    @Column(nullable = false, length = 50)
     private String email;
+
+    @Column(nullable = false, length = 10)
     private String role; // USER(고객), SELLER(판매자), ADMIN(관리자)
+
+    @Column(nullable = false, length = 10)
+    private Boolean status; // TRUE 활성 FALSE 비활성
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    // 권한 변경 (관리자)
+    public void updateRole(String role) {
+        if (this.role.equals(role)) {
+            // checkpoint 동일한 권한으로 변경할 수 없습니다.
+        }
+        this.role = role;
+    }
+
+    // 회원 탈퇴
+    public void delete() {
+        this.status = false;
+    }
 
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
-
     @PreUpdate
     protected void onUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
-
     @Builder
-    public User(Long id, String username, String password, String email, String role, LocalDateTime createdAt) {
+    public User(Long id, String username, String password, String email, String role, Boolean status, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
         this.role = role;
+        this.status = status;
         this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
