9b30436a587b0a4f47cf08c2038421782f1f3977
metacoding <getinthere@naver.com>
2023-04-12 22:00:49 +0900

    주석 달기

--- src/main/java/shop/mtcoding/metamall/dto/order/OrderRequest.java
+++ src/main/java/shop/mtcoding/metamall/dto/order/OrderRequest.java
@@ -22,16 +22,21 @@ public class OrderRequest {
             private Integer count;
         }
 
+        // 1. request 요청으로 들어온 product id만 리스트로 뽑아내기
         public List<Long> getIds() {
             return orderProducts.stream().map((orderProduct) -> orderProduct.getProductId()).collect(Collectors.toList());
         }
 
-        public List<OrderProduct> toEntity(List<Product> products) {
+        // 3. 주문 상품 리스트 만들어 내기
+        public List<OrderProduct> toEntity(List<Product> products) { // 2. getIds() 로 뽑아낸 번호로 상품 리스트 찾아내서 주입하기
 
+            // 4. request 요청으로 들어온 DTO에 count 값이 필요해서 stream() 두번 돌렸음. 주문 상품 금액을 만들어 내야 해서!!
             return orderProducts.stream()
+                    // 5. map은 다시 collect로 수집해야 하기 때문에, flatMap으로 평탄화 작업함.
                     .flatMap((orderProduct) -> {
                                 Long productId = orderProduct.productId;
                                 Integer count = orderProduct.getCount();
+                                // 6. OrderProduct 객체 만들어내기 (주문한 상품만큼)
                                 return products.stream()
                                         .filter((product) -> product.getId().equals(productId))
                                         .map((product) -> OrderProduct.builder()
@@ -40,7 +45,7 @@ public class OrderRequest {
                                                 .orderPrice(product.getPrice() * count)
                                                 .build());
                             }
-                    ).collect(Collectors.toList());
+                    ).collect(Collectors.toList()); // 7. 최종 수집
         }
     }
 }
