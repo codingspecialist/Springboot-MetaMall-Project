9f193a39b654a270b92c2475863ed53754099d16
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-04-14 16:16:26 +0900

    더미데이터 및 회원가입 추가

--- src/main/java/shop/mtcoding/metamall/controller/UserController.java
+++ src/main/java/shop/mtcoding/metamall/controller/UserController.java
@@ -2,13 +2,14 @@ package shop.mtcoding.metamall.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import shop.mtcoding.metamall.core.exception.Exception400;
 import shop.mtcoding.metamall.core.exception.Exception401;
 import shop.mtcoding.metamall.core.jwt.JwtProvider;
-import shop.mtcoding.metamall.dto.ResponseDto;
+import shop.mtcoding.metamall.dto.ResponseDTO;
 import shop.mtcoding.metamall.dto.user.UserRequest;
-import shop.mtcoding.metamall.dto.user.UserResponse;
 import shop.mtcoding.metamall.model.log.login.LoginLog;
 import shop.mtcoding.metamall.model.log.login.LoginLogRepository;
 import shop.mtcoding.metamall.model.user.User;
@@ -19,6 +20,9 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+/**
+ * 회원가입, 로그인
+ */
 @RequiredArgsConstructor
 @RestController
 public class UserController {
@@ -27,15 +31,36 @@ public class UserController {
     private final LoginLogRepository loginLogRepository;
     private final HttpSession session;
 
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO joinDTO) {
+        if (joinDTO.getUsername().isBlank()) {
+            throw new Exception400("username을 입력해주세요.");
+        }
+        if (joinDTO.getPassword().isBlank()) {
+            throw new Exception400("password를 입력해주세요.");
+        }
+        if (joinDTO.getEmail().isBlank()) {
+            throw new Exception400("email을 입력해주세요.");
+        }
+        if (joinDTO.getRole().isBlank()) {
+            throw new Exception400("role을 입력해주세요.");
+        }
+
+        User userPS = userRepository.save(joinDTO.toEntity());
+
+        ResponseDTO<?> responseDTO = new ResponseDTO<>().data(userPS);
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequest.LoginDto loginDto, HttpServletRequest request) {
-        Optional<User> userOP = userRepository.findByUsername(loginDto.getUsername());
+    public ResponseEntity<?> login(@RequestBody UserRequest.LoginDTO loginDTO, HttpServletRequest request) {
+        Optional<User> userOP = userRepository.findByUsername(loginDTO.getUsername());
         if (userOP.isPresent()) {
             // 1. 유저 정보 꺼내기
             User loginUser = userOP.get();
 
             // 2. 패스워드 검증하기
-            if(!loginUser.getPassword().equals(loginDto.getPassword())){
+            if (!loginUser.getPassword().equals(loginDTO.getPassword())) {
                 throw new Exception401("인증되지 않았습니다");
             }
 
@@ -54,7 +79,7 @@ public class UserController {
             loginLogRepository.save(loginLog);
 
             // 6. 응답 DTO 생성
-            ResponseDto<?> responseDto = new ResponseDto<>().data(loginUser);
+            ResponseDTO<?> responseDto = new ResponseDTO<>().data(loginUser);
             return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body(responseDto);
         } else {
             throw new Exception400("유저네임 혹은 아이디가 잘못되었습니다");
--- src/main/java/shop/mtcoding/metamall/core/exception/Exception500.java
+++ src/main/java/shop/mtcoding/metamall/core/exception/Exception500.java
@@ -2,7 +2,7 @@ package shop.mtcoding.metamall.core.exception;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import shop.mtcoding.metamall.dto.ResponseDto;
+import shop.mtcoding.metamall.dto.ResponseDTO;
 
 
 // 서버 에러
@@ -12,8 +12,8 @@ public class Exception500 extends RuntimeException {
         super(message);
     }
 
-    public ResponseDto<?> body(){
-        ResponseDto<String> responseDto = new ResponseDto<>();
+    public ResponseDTO<?> body(){
+        ResponseDTO<String> responseDto = new ResponseDTO<>();
         responseDto.fail(HttpStatus.INTERNAL_SERVER_ERROR, "serverError", getMessage());
         return responseDto;
     }
