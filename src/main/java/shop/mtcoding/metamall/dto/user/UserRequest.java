576ef00b8ecc7e06aac7bc5747f2bc8879ca6743
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-17 11:18:54 +0900

    FEAT: add UserRequest JoinDto with regexp

--- src/main/java/shop/mtcoding/metamall/dto/user/UserRequest.java
+++ src/main/java/shop/mtcoding/metamall/dto/user/UserRequest.java
@@ -3,12 +3,35 @@ package shop.mtcoding.metamall.dto.user;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserRequest {
     @Getter @Setter
     public static class LoginDto {
+        @NotEmpty
         private String username;
+        @NotEmpty
         private String password;
     }
 
+    @Getter @Setter
+    public static class JoinDto {
+        @NotEmpty
+        @Size(min = 3, max = 20)
+        private String username;
 
+        @NotEmpty
+        @Size(min = 4, max = 20) // DB에는 60자가 들어가겠지만 여기서는 20자!
+        private String password;
+
+        @NotEmpty
+        @Pattern(regexp =  "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식이 아닙니다.")
+        private String email;
+
+        @NotEmpty
+        @Pattern(regexp = "USER|SELLER|ADMIN")
+        private String role;
+    }
 }
