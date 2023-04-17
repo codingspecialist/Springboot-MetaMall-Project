5c39507bb1b495aa164d2fdd287809bb4b0ad8b4
yeeeeerim <gyr3904@gmail.com>
2023-04-17 15:57:01 +0900

    오류수정

--- src/main/java/shop/mtcoding/metamall/util/JwtUtil.java
+++ src/main/java/shop/mtcoding/metamall/util/JwtUtil.java
@@ -1,6 +1,5 @@
 package shop.mtcoding.metamall.util;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,27 +7,27 @@ import org.springframework.stereotype.Component;
 * */
 @Component
 public class JwtUtil {
-
-	private static final String SECRET = "메타코딩";
-	private static final String AUTH_HEADER = "Authorization";
-	private static final String TOKEN_PREFIX = "Bearer ";
-
-	public String extractUsername(String token) {
-		Claims claims = Jwts.parser()
-				.setSigningKey(SECRET)
-				.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-				.getBody();
-
-		return claims.getSubject();
-	}
-
-	public String extractRole(String token) {
-		Claims claims = Jwts.parser()
-				.setSigningKey(SECRET)
-				.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-				.getBody();
-
-		return claims.get("role", String.class);
-	}
+//
+//	private static final String SECRET = "메타코딩";
+//	private static final String AUTH_HEADER = "Authorization";
+//	private static final String TOKEN_PREFIX = "Bearer ";
+//
+//	public String extractUsername(String token) {
+//		Claims claims = Jwts.parser()
+//				.setSigningKey(SECRET)
+//				.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+//				.getBody();
+//
+//		return claims.getSubject();
+//	}
+//
+//	public String extractRole(String token) {
+//		Claims claims = Jwts.parser()
+//				.setSigningKey(SECRET)
+//				.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+//				.getBody();
+//
+//		return claims.get("role", String.class);
+//	}
 }
 
