08d9cff1e68bfb778748810f3e7e4125b0637dba
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-17 14:05:29 +0900

    FEAT: add product controller 상ífindAll method

--- src/main/java/shop/mtcoding/metamall/controller/ProductController.java
+++ src/main/java/shop/mtcoding/metamall/controller/ProductController.java
@@ -1,11 +1,13 @@
 package shop.mtcoding.metamall.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.metamall.core.annotation.MySessionStore;
 import shop.mtcoding.metamall.core.exception.Exception400;
 import shop.mtcoding.metamall.core.session.SessionUser;
@@ -33,6 +35,7 @@ public class ProductController {
     /**
      * 상품 등록
      */
+    // seller인지 아닌지는 할 필요 없다. -> 인터셉터가 판매자인지 권한 체크를 하니까
     @PostMapping("/seller/products") // 앞에 seller가 있다? -> interceptor가 권한 판단
     public ResponseEntity<?> save(@RequestBody @Valid ProductRequest.SaveDTO saveDTO, Errors errors, @MySessionStore SessionUser sessionUser) {
         // 1. 판매자 찾기
@@ -45,4 +48,35 @@ public class ProductController {
         ResponseDTO<?> responseDTO = new ResponseDTO<>().data(productPS);
         return ResponseEntity.ok().body(responseDTO);
     }
+
+    /**
+     * 상품 등록
+     */
+    // http://localhost:8080/products?page=1
+    @GetMapping("/products")
+    public ResponseEntity<?> findAll(@PageableDefault(size = 10, page = 0, direction = Sort.Direction.DESC) Pageable pageable){
+        // 1. 상품 찾기
+        Page<Product> productPagePS = productRepository.findAll(pageable);
+
+        // 2. 응답하기
+        ResponseDTO<?> responseDto = new ResponseDTO<>().data(productPagePS);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    /**
+     * 상품목록보기
+     */
+
+
+    /**
+     * 상품상세보기
+     */
+
+    /**
+     * 상품수정하기
+     */
+
+    /**
+     * 상품삭제하기
+     */
 }
