d263693a510c37b1f8fe15fe5a121762dc2f02b7
Kayden <dongw777@gmail.com>
2023-04-18 15:15:19 +0900

    Dummy Data  세팅 완료

--- src/main/java/shop/mtcoding/metamall/MetamallApplication.java
+++ src/main/java/shop/mtcoding/metamall/MetamallApplication.java
@@ -10,16 +10,21 @@ import shop.mtcoding.metamall.model.product.ProductRepository;
 import shop.mtcoding.metamall.model.user.User;
 import shop.mtcoding.metamall.model.user.UserRepository;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MetamallApplication {
 
+	// CommandLineRunner 는 Spring Boot 가 최초 실행될 때 return 값을 실행시켜 줌
 	@Bean
 	CommandLineRunner initData(UserRepository userRepository, ProductRepository productRepository, OrderProductRepository orderProductRepository, OrderSheetRepository orderSheetRepository){
 		return (args)->{
 			// 여기에서 save 하면 됨.
 			// bulk Collector는 saveAll 하면 됨.
-			User ssar = User.builder().username("ssar").password("1234").email("ssar@nate.com").role("USER").build();
-			userRepository.save(ssar);
+			User ssar = User.builder().username("ssar").password("1234").email("ssar@nate.com").role("USER").status(true).build();
+			User seller = User.builder().username("seller").password("1234").email("seller@nate.com").role("SELLER").status(true).build();
+			User admin = User.builder().username("admin").password("1234").email("admin@nate.com").role("ADMIN").status(true).build();
+			userRepository.saveAll(Arrays.asList(ssar, seller, admin));	// 벌크 컬렉터로 save 시에는 saveAll 사용(컬렉션 들어가야 함)
 		};
 	}
 
