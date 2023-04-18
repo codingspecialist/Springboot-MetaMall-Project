a0ead35b2968e1dc5bf69e79247df1366d5c0185
Kayden <dongw777@gmail.com>
2023-04-18 17:38:39 +0900

    Core 세팅 완ㄹ

--- src/main/java/shop/mtcoding/metamall/core/advice/MyExceptionAdvice.java
+++ src/main/java/shop/mtcoding/metamall/core/advice/MyExceptionAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import shop.mtcoding.metamall.core.annotation.MyErrorLogRecord;
 import shop.mtcoding.metamall.core.exception.*;
 import shop.mtcoding.metamall.dto.ResponseDTO;
 import shop.mtcoding.metamall.model.log.error.ErrorLogRepository;
@@ -16,24 +17,32 @@ import shop.mtcoding.metamall.model.log.error.ErrorLogRepository;
 @RestControllerAdvice
 public class MyExceptionAdvice {
 
+    @MyErrorLogRecord
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e){
-
+        // trace -> debug -> info -> warn -> error
+        log.debug("디버그: " + e.getMessage());
+        log.info("인포: " + e.getMessage());
+        log.warn("경고: " + e.getMessage());
+        log.error("에러: " + e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
+    @MyErrorLogRecord
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> unAuthorized(Exception401 e){
 
         return new ResponseEntity<>(e.body(), e.status());
     }
 
+    @MyErrorLogRecord
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> forbidden(Exception403 e){
 
         return new ResponseEntity<>(e.body(), e.status());
     }
 
+    @MyErrorLogRecord
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<?> notFound(NoHandlerFoundException e){
         ResponseDTO<String> responseDto = new ResponseDTO<>();
@@ -41,6 +50,7 @@ public class MyExceptionAdvice {
         return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
     }
 
+    @MyErrorLogRecord
     // 나머지 모든 예외는 이 친구에게 다 걸리짐
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e){
